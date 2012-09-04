a35efc23869da6319d63e7ba1e76e18bfe0ff0cc
Kyle Wilcox <kwilcox@asascience.com>
2012-09-04 13:39:43 -0400

    No more need for gunicorn config file

--- gunicorn.conf.py
+++
@@ -1,9 +0,0 @@
-# Gunicorn configuration for production environment
-
-bind = "127.0.0.1:8000"
-workers = 2
-debug = True
-daemon = False
-pidfile = "gunicorn.pid"
-logfile = "gunicorn.log"
-backlog = 2048

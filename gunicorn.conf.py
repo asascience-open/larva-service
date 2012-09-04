430781e9b03c7e8fcc9f12268a14f4d0af2724cc
Kyle Wilcox <kwilcox@asascience.com>
2012-09-04 12:53:09 -0400

    Don't daemonize gunicorn, as we are using Foreman for process management

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,8 +1,9 @@
 # Gunicorn configuration for production environment
 
-bind = "127.0.0.1:4000"
+bind = "127.0.0.1:8000"
 workers = 2
 debug = True
-daemon = True
+daemon = False
 pidfile = "gunicorn.pid"
 logfile = "gunicorn.log"
+backlog = 2048

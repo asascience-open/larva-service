363f9ac65a3a575a105f21d4140b59cebdc52e15
Kyle Wilcox <kwilcox@asascience.com>
2012-10-04 10:47:17 -0400

    Add an OUTPUT_PATH to testing configuration

--- larva_service/testing.py
+++ larva_service/testing.py
@@ -16,3 +16,4 @@ S3_BUCKET = "larvamap-testing"
 
 BATHY_PATH = "/home/dev/Development/paegan/paegan/resources/bathymetry/ETOPO1_Bed_g_gmt4.grd"
 CACHE_PATH = "/home/dev/Development/larva-service/cache"
+OUTPUT_PATH = "/home/dev/Development/larva-service/output"

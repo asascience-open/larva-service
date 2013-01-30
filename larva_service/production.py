6a6ad9f5db72a31bc9c005b4335e44bc4c6e213c
Kyle Wilcox <kwilcox@asascience.com>
2013-01-30 13:45:06 -0500

    Update production to save output to scratch area

--- larva_service/production.py
+++ larva_service/production.py
@@ -3,7 +3,7 @@ LOG_FILE = True
 
 S3_BUCKET = "larvamap-production"
 
-BATHY_PATH = "/data/bathy/ETOPO1_Bed_g_gmt4.grd"
-CACHE_PATH = "/data/cache"
-OUTPUT_PATH = "/data/output"
-SHORE_PATH = "/data/shore/New_Land_Clean.shp"
+BATHY_PATH = "/data/bathy/world_etopo1/ETOPO1_Bed_g_gmt4.grd"
+CACHE_PATH = "/scratch/cache"
+OUTPUT_PATH = "/scratch/output"
+SHORE_PATH = "/data/shore/westcoast/New_Land_Clean.shp"

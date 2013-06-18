75c6951bb6c6a4f1e8767735bf8c85c74234bdc5
Kyle Wilcox <kwilcox@asascience.com>
2013-06-17 23:04:24 -0400

    Add a path_type to a Shoreline

--- larva_service/models/shoreline.py
+++ larva_service/models/shoreline.py
@@ -1,6 +1,6 @@
 import json
 import os
-from flask.ext.mongokit import Document
+from mongokit import Document, DocumentMigration
 from larva_service import app, db, redis_connection
 from datetime import datetime
 from shapely.geometry import Point, asShape, Polygon, MultiPolygon
@@ -9,16 +9,20 @@ from paegan.transport.shoreline import Shoreline as PTShoreline
 from shapely.geometry import box
 from rq.job import Job
 
+class RunMigration(DocumentMigration):
+    def allmigration01__add_path_type_field(self):
+        self.target = {'path_type':{'$exists': False}}
+        self.update = {'$set':{'path_type':u''}}
+
 class Shoreline(Document):
     __collection__ = 'shorelines'
     use_dot_notation = True
     structure = {
         'name'              : unicode,  # Name of the shoreline
         'path'              : unicode,  # url to WFS server or file name on disk
-        'feature_name'      : unicode,  # feature name in WFS server
+        'path_type'         : unicode,  # WFS or Shapefile class
+        'feature_name'      : unicode,  # feature name in WFS server or layer in Shapefile
         'title'             : unicode,  # title of the shoreline in WFS
-        'spatialbuffer'     : float,
-        'react_type'        : unicode,  # "reverse", "bounce", or other for no movement
         'bbox'              : unicode,  # WKT of the bounding box
         'geometry'          : unicode,  # WKT of the bounding polygon (unused currently)
         'task_id'           : unicode,  # id of import task
@@ -28,6 +32,7 @@ class Shoreline(Document):
     default_values = {
                       'created': datetime.utcnow
                       }
+    migration_handler = RunMigration
 
     def status(self):
         if Job.exists(self.task_id, connection=redis_connection):
@@ -41,6 +46,7 @@ class Shoreline(Document):
         s = PTShoreline(path=self.path, feature_name=self.feature_name)
         caps = s.get_feature_type_info()
 
+        self.path_type = unicode(type(s).__name__)
         if caps is not None:
             self.bbox  = unicode(caps['LatLongBoundingBox'].wkt)
             self.title = unicode(caps['Name'])
